e3d8fe99fd8e96882aacd4c48e52cc7b4b4c7bab
Alexander Koltun <alexander.koltun@gmail.com>
2014-12-17 19:00:38 +0000

    Add acceptance spec for answering question

---
+++ spec/features/answer/answer_question_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'User answers question', %q{
+  In order to help others
+  As an user
+  I want to be able to answer question
+} do
+  given(:question) { create(:question) }
+  given(:answer) { 'Answer body' }
+  background { Capybara.match = :first }
+
+  scenario 'User answers question' do
+    visit question_path(question)
+    fill_in 'Answer', with: :answer
+    click_on 'Save Answer'
+
+    expect(current_path).to eq question_path(question)
+    expect(page).to have_content 'You have created a new answer'
+    expect(page).to have_content :question
+  end
+
+  scenario 'User saves invalid answer' do
+    visit question_path(question)
+    click_on 'Save Answer'
+
+    expect(current_path).to eq question_answers_path(question)
+    expect(page).to have_content 'Error!'
+    expect(page).to have_css '.field_with_errors'
+  end
+end
--- spec/features/question/list_questions_spec.rb
+++ spec/features/question/list_questions_spec.rb
@@ -5,16 +5,16 @@ feature 'User lists questions', %q{
   As an user
   I want to be able to list all questions
 } do
-  given(:titles) { ['Title 1', 'Title 2'] }
-  given(:questions) { ['Body1', 'Body 2'] }
+  given(:questions) { [{ title: 'Title 1', question: 'Body 1' }, { title: 'Title 2', question: 'Body 2' }] }
+  background { questions.each { |question| create(:question, question) } }
 
   scenario 'User lists all questions' do
     visit questions_path
 
     expect(current_path).to eq questions_path
-    expect(page).to have_content :titles[0]
-    expect(page).to have_content :questions[0]
-    expect(page).to have_content :titles[1]
-    expect(page).to have_content :questions[1]
+    expect(page).to have_content questions[0][:title]
+    expect(page).to have_content questions[0][:question]
+    expect(page).to have_content questions[1][:title]
+    expect(page).to have_content questions[1][:question]
   end
 end
