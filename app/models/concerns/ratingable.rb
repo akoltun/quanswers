582b98c751226e9c73be81123455194bf5014c9a
Alexander Koltun <alexander.koltun@gmail.com>
2015-02-05 05:04:12 +0300

    Small refactoring

--- app/models/concerns/ratingable.rb
+++ app/models/concerns/ratingable.rb
@@ -1,11 +1,12 @@
 module Ratingable
   def rating!(user, value)
     return if value.nil?
+
     current_rating = ratings.where(user: user).first
     if current_rating
       current_rating.update!(rating: value)
     else
-      ratings.create!(user: user, rating: value).nil?
+      ratings.create!(user: user, rating: value)
     end
     update!(rating: ratings.average(:rating).round(1))
   end
